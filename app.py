8e3b9e2f5576a2c78e36ae3bc23c704aa7fe359f
Pawel Suchanecki <subdcc@gmail.com>
2023-08-24 03:15:32 +0200

    Allow querying by 1+ chars, some optimisations and comments.
    Allow querying by 1+ chars.
    Eliminating one string comparison.
    Added comment.

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 # Pawel Suchanecki <psuchanecki@almalinux.org>
 from fastapi import FastAPI
 import pandas as pd
-#from typing import List
 import time
 
-# Benchmark?
+# Do benchmark?
 b = True
 
 app = FastAPI()
 
 # Load CSV file into a Pandas DataFrame
 data = pd.read_csv("file.csv")
+
+# Build the list of columns from first row of file.csv
 columns = data.columns.tolist()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the AlmaLinux Driver List Search API"}
-
 
 @app.get("/search/")
 def search(query: str = ""):
@@ -26,25 +26,22 @@ def search(query: str = ""):
     if query == "":
         result = {"results": data.to_dict(orient="records")}
     else:
-        if len(query) < 3:
-            result = {"results": []}
-        else:
-            search_query = query.strip().lower()
-            mask = False
+        search_query = query.strip().lower()
+        mask = False
 
-            for column in columns:
-                mask |= data[column].astype(str).str.contains(search_query, case=False)
-                #  Break the search on first column that matches -- great optimization! :)
-                if mask.any():
-                    break
+        for column in columns:
+            mask |= data[column].astype(str).str.contains(search_query, case=False)
+            #  Break the search on first column that matches -- great optimization! :)
+            if mask.any():
+                break
 
-            search_result = data[mask]
-            result = {"results": search_result.to_dict(orient="records")}
+        search_result = data[mask]
+        result = {"results": search_result.to_dict(orient="records")}
 
     if b:
         end_time = time.time() # End the timer
         elapsed_time = end_time - start_time
 
-        print(f"PERF:\t  Query served in {elapsed_time:.6f} seconds") # Print the elapsed time to console
+        print(f"PERF:\t  Query served in {elapsed_time*1000:.1f} miliseconds") # Print the elapsed time to console
 
     return result
